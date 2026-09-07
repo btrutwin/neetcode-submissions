1d3b65fb28da8662564bdef411037cc1576e9132
Ben Trutwin <156606871+btrutwin@users.noreply.github.com>
2026-09-07 18:54:10 -0500

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        // need to make decreasing stack. add element. Check if element before is smaller.
+        // if so, loop through stack, popping until empty or bigger element is found
+        // only store index in stack, but compare with temp[i]
+        // stack should be in decreasing order when index is called
+
+        Stack<Integer> tempIdx = new Stack<>();
+        int[] result = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+                while (!tempIdx.isEmpty() && temperatures[i] > temperatures[tempIdx.peek()]) {
+                    result[tempIdx.peek()] = i - tempIdx.pop();
+                }
+            
+
+            tempIdx.push(i);
+        }
+
+        while (!tempIdx.isEmpty()) {
+            result[tempIdx.pop()] = 0;
+        }
+
+        return result;
+    }
+}
