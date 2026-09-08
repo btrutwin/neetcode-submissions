fde284e7674bb9edad978ea084d65b1a8a9bfa65
Ben Trutwin <156606871+btrutwin@users.noreply.github.com>
2026-09-08 11:49:50 -0500

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Stack<Double> stack = new Stack<>();
+        int n = position.length;
+        int[][] cars = new int[n][2];
+
+        for(int i = 0; i < n; i++){
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> b[0] - a[0]);
+
+        for(int i = 0; i < n; i++){
+            double time = (double) (target - cars[i][0]) / (cars[i][1]);
+            if(stack.isEmpty() || stack.peek() < time){
+                stack.push(time);
+            }
+        }
+        return stack.size();
+    }
+}
