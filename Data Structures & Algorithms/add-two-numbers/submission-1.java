2129a7a0cdb557f5f8ef2d383ac6e55650aa7121
Ben Trutwin <156606871+btrutwin@users.noreply.github.com>
2026-09-26 11:22:35 -0500

    Add: add-two-numbers - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int value = carry;
+
+            if (l1 != null) {
+                value += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                value += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = value / 10;
+            int digit = value % 10;
+
+            curr.next = new ListNode(digit);
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
